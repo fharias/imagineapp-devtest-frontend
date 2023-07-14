1d7219937adda4109e06fef7865c8222720f9757
FABIO ARIAS <fharias@gmail.com>
2023-07-14 13:42:01 -0500

    New POst component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { NewPostComponent } from './new-post/new-post.component';
 
 const routes: Routes = [
   {
@@ -12,7 +13,10 @@ const routes: Routes = [
     path: 'signup',
     component: SignupComponent,
   },
-
+  {
+    path: 'newpost',
+    component: NewPostComponent,
+  },
   {
     path: '**',
     redirectTo: 'login',
--- src/app/new-post/new-post.component.ts
+++ src/app/new-post/new-post.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-post.component.css']
 })
 export class NewPostComponent {
-
+  maxTitleLength = 30;
+  maxContentLength = 300;
+  currentDate = new Date().toISOString().slice(0, 10);  
 }
